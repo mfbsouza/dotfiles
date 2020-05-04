7a4e5d45df9a8e1d4b233ae1a03d54dcb6993082
mfbsouza <mfbs@cin.ufpe.br>
2020-05-04 03:47:59 -0300

    updated optimus script for Gnome Launch

--- scripts/nvidia-drivers.sh
+++ scripts/nvidia-drivers.sh
@@ -4,4 +4,4 @@ echo "Installing Nvidia Graphics drivers..."
 sudo pacman -S nvidia\
        	nvidia-utils lib32-nvidia-utils\
 	nvidia-settings\
-       	opencl-nvidia ocl-icd opencl-headers cuda --noconfirm
+       	opencl-nvidia ocl-icd opencl-headers --noconfirm
--- scripts/optimus.sh
+++ scripts/optimus.sh
@@ -8,3 +8,5 @@ yay -S optimus-manager --noconfirm
 sudo cp ../etc/optimus-manager/optimus-manager.conf /etc/optimus-manager/
 
 sudo pacman -S nvidia-prime --noconfirm
+yay -S switcheroo-control --noconfirm
+sudo systemctl enable switcheroo-control
