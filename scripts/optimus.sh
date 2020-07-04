068549271eba09fe9c8898087ee54709d344beda
mfbsouza <mfbs@cin.ufpe.br>
2020-07-04 11:14:50 -0300

    updated chromium vaapi flags & optimus manager conf

--- scripts/optimus.sh
+++ scripts/optimus.sh
@@ -5,6 +5,6 @@ echo "Setting up for Nvidia Optimus..."
 sudo pacman -S bbswitch --noconfirm
 yay -S optimus-manager --noconfirm
 
-sudo cp ../etc/optimus-manager/optimus-manager.conf /etc/optimus-manager/
+sudo cp optimus-manager.conf /etc/optimus-manager/
 
 sudo pacman -S nvidia-prime --noconfirm
