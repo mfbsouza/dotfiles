fb4c47d5e45e4fe81017035eb40c7ba30584d6e2
mfbsouza <mfbs@cin.ufpe.br>
2020-10-14 09:53:00 -0300

    wrote new setup script in python

---
+++ setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+from os import getcwd, listdir, environ, symlink, remove
+from os.path import isfile, isdir, join
+from shutil import rmtree
+
+current_path = getcwd()
+current_config_path = join(current_path, '.config')
+
+home_path = environ['HOME']
+default_config_path = join(home_path, '.config')
+
+ignore = ['README.md', 'setup.py', '.git']
+
+for element in listdir(current_path):
+    if isfile(join(current_path, element)) and element not in ignore:
+        try:
+            symlink(join(current_path, element), join(home_path, element))
+        except FileExistsError:
+            remove(join(home_path, element))
+            symlink(join(current_path, element), join(home_path, element))
+
+
+for element in listdir(current_config_path):
+    if isdir(join(current_config_path, element)) and element not in ignore:
+        try:
+            symlink(join(current_config_path, element), join(default_config_path, element))
+        except FileExistsError:
+            rmtree(join(default_config_path, element))
+            symlink(join(current_config_path, element), join(default_config_path, element))
+
+
+print("finished...")
