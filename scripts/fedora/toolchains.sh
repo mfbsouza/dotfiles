febd81e426eb8fe8916f5d85eab1dd319bb11874
mfbsouza <mfbsouza.it@gmail.com>
2022-03-04 18:16:07 -0300

    scripts/fedora/toolchains.sh: added vundle install and ycm deps

--- scripts/fedora/toolchains.sh
+++ scripts/fedora/toolchains.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
 sudo dnf -y install vulkan-headers vulkan-tools arm-none-eabi-gcc avr-gcc avrdude avr-libc \
-	python3-pip cmake gcc-c++ python3-devel
+	python3-pip cmake gcc-c++ python3-devel bear
+
+echo "open vim to download YCM with :PlugInstall and"
+echo "$ cd ~/.vim/bundle/YouCompleteMe"
+echo "$ python3 install.py --clangd-completer"
