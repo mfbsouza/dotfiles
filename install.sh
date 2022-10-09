4ca5b3ca732ea13e48e0c096a7a04091db491c84
mfbsouza <mfbsouza.it@gmail.com>
2022-10-09 12:32:20 -0300

    install.sh: fixed colors not showing properly in echo mesg
    Signed-off-by: mfbsouza <mfbsouza.it@gmail.com>

--- install.sh
+++ install.sh
@@ -2,16 +2,17 @@
 
 GREEN='\033[0;32m'
 CLEAR='\033[0m'
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo "$GREEN Copying neovim config folder $CLEAR"
+echo -e "$GREEN Copying neovim config folder $CLEAR"
 cp -r $SCRIPT_DIR/.config/nvim ~/.config/
 
-echo "$GREEN Copying MangoHud config folder $CLEAR"
+echo -e "$GREEN Copying MangoHud config folder $CLEAR"
 cp -r $SCRIPT_DIR/.config/MangoHud ~/.config/
 
-echo "$GREEN Copying vkBasalt config folder $CLEAR"
+echo -e "$GREEN Copying vkBasalt config folder $CLEAR"
 cp -r $SCRIPT_DIR/.config/vkBasalt ~/.config/
 
-echo "$GREEN Done. $CLEAR"
+echo -e "$GREEN Done. $CLEAR"
 
