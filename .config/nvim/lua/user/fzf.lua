6abd314c85b72602059aa3939f9bfa6ce1ee778c
mfbsouza <mfbsouza.it@gmail.com>
2022-10-13 22:37:34 -0300

    neovim: updated telescope theme
    Signed-off-by: mfbsouza <mfbsouza.it@gmail.com>

--- .config/nvim/lua/user/fzf.lua
+++ .config/nvim/lua/user/fzf.lua
@@ -9,3 +9,27 @@ vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
+
+-- telescope theme
+require('telescope').setup{
+  defaults = {
+    -- ...
+  },
+  pickers = {
+    find_files = {
+      theme = "dropdown",
+    },
+    live_grep = {
+      theme = "dropdown",
+    },
+    buffers = {
+      theme = "dropdown",
+    },
+    help_tags = {
+      theme = "dropdown",
+    },
+  },
+  extensions = {
+    -- ...
+  }
+}
