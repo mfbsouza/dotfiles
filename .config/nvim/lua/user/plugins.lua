1cba2dd6b6cce7587e1bea252bbf067329f3e9ed
mfbsouza <mfbsouza.it@gmail.com>
2022-10-17 19:11:45 -0300

    neovim: changed theme to tokyo night
    Signed-off-by: mfbsouza <mfbsouza.it@gmail.com>

--- .config/nvim/lua/user/plugins.lua
+++ .config/nvim/lua/user/plugins.lua
@@ -45,7 +45,7 @@ return packer.startup(function(use)
   use "nvim-lua/popup.nvim"
 
   -- colorschemes
-  use "lunarvim/colorschemes"
+  use "folke/tokyonight.nvim"
 
   -- completion engine
   use "hrsh7th/nvim-cmp"
