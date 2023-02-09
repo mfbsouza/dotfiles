257c0e94926d3d562ce25cb297dbfa3b0c5416c7
mfbsouza <mfbsouza.it@gmail.com>
2023-02-09 17:29:13 -0300

    Uptaded install script to copy gitconfig to the home folder

--- install.sh
+++ install.sh
@@ -5,6 +5,9 @@ CLEAR='\033[0m'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+echo -e "$GREEN Copying gitconfig to home $CLEAR"
+cp $SCRIPT_DIR/.gitconfig ~/
+
 echo -e "$GREEN Copying neovim config folder $CLEAR"
 cp -r $SCRIPT_DIR/.config/nvim ~/.config/
 
