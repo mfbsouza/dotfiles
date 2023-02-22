a5feaa5404a8485c1690358edb137be78a980b2e
mfbsouza <mfbsouza.it@gmail.com>
2023-02-22 11:00:58 -0300

    Created tmux config file and update install script to linking instead of copying

--- install.sh
+++ install.sh
@@ -5,17 +5,23 @@ CLEAR='\033[0m'
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-echo -e "$GREEN Copying gitconfig to home $CLEAR"
-cp $SCRIPT_DIR/.gitconfig ~/
+echo -e "$GREEN Linking gitconfig to home $CLEAR"
+ln -s $SCRIPT_DIR/.gitconfig ~/.gitconfig
 
-echo -e "$GREEN Copying neovim config folder $CLEAR"
-cp -r $SCRIPT_DIR/.config/nvim ~/.config/
+echo -e "$GREEN Linking tmux config to home $CLEAR"
+ln -s $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
 
-echo -e "$GREEN Copying MangoHud config folder $CLEAR"
-cp -r $SCRIPT_DIR/.config/MangoHud ~/.config/
+echo -e "$GREEN Linking vimrc config to home $CLEAR"
+ln -s $SCRIPT_DIR/.vimrc ~/.vimrc
 
-echo -e "$GREEN Copying vkBasalt config folder $CLEAR"
-cp -r $SCRIPT_DIR/.config/vkBasalt ~/.config/
+echo -e "$GREEN Linking neovim config folder $CLEAR"
+ln -s $SCRIPT_DIR/.config/nvim ~/.config/nvim
+
+echo -e "$GREEN Linking MangoHud config folder $CLEAR"
+ln -s $SCRIPT_DIR/.config/MangoHud ~/.config/MangoHud
+
+echo -e "$GREEN Linking vkBasalt config folder $CLEAR"
+ln -s $SCRIPT_DIR/.config/vkBasalt ~/.config/vkBasalt
 
 echo -e "$GREEN Done. $CLEAR"
 
