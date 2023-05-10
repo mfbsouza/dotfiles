0e68257e0955ee2ea4a13e331a10ce67e9743afd
mfbsouza <mfbsouza.it@gmail.com>
2023-05-10 00:48:45 -0300

    server: update packages script

--- scripts/server/packages.sh
+++ scripts/server/packages.sh
@@ -4,9 +4,7 @@ GREEN='\033[0;32m'
 RED='\033[0;31m'
 CLEAR='\033[0m'
 
-echo ""
 echo -e "$GREEN Updating the system packages... $CLEAR"
-echo ""
 
 sudo apt update && sudo apt upgrade
 if [ "$?" -ne 0 ]; then
@@ -16,11 +14,9 @@ if [ "$?" -ne 0 ]; then
 	exit 1
 fi
 
-echo ""
 echo -e "$GREEN Installing Packages... $CLEAR"
-echo ""
 
-sudo apt install neofetch docker.io
+sudo apt install docker.io ninja-build gettext cmake unzip
 if [ "$?" -ne 0 ]; then
 	echo ""
 	echo -e "$RED Something went wrong! Stopping... $CLEAR"
@@ -28,29 +24,7 @@ if [ "$?" -ne 0 ]; then
 	exit 1
 fi
 
-echo ""
-echo -e "$GREEN Installing ripgrep... $CLEAR"
-echo ""
-
-wget https://github.com/BurntSushi/ripgrep/releases/download/13.0.0/ripgrep_13.0.0_amd64.deb
-if [ "$?" -ne 0 ]; then
-	echo ""
-	echo -e "$RED Something went wrong! Stopping... $CLEAR"
-	echo ""
-	exit 1
-fi
-sudo dpkg -i ripgrep_13.0.0_amd64.deb
-if [ "$?" -ne 0 ]; then
-	echo ""
-	echo -e "$RED Something went wrong! Stopping... $CLEAR"
-	echo ""
-	exit 1
-fi
-rm ripgrep_13.0.0_amd64.deb
-
-echo ""
 echo -e "$GREEN Adding user to groups... $CLEAR"
-echo ""
 
 sudo usermod -a -G dialout $USER
 if [ "$?" -ne 0 ]; then
@@ -67,7 +41,5 @@ if [ "$?" -ne 0 ]; then
 	exit 1
 fi
 
-echo ""
 echo -e "$GREEN Done! $CLEAR"
-echo ""
 exit 0
