d09460506c6702f86b3fa7c0201af0645b10a2af
mfbsouza <mfbsouza.it@gmail.com>
2023-05-12 15:30:32 -0300

    install.sh: more complete install script

--- install.sh
+++ install.sh
@@ -3,41 +3,52 @@
 GREEN='\033[0;32m'
 CLEAR='\033[0m'
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$ROOT_DIR/scripts
 
-echo "Install Desktop or Server config files? [d/s]"
+echo "$GREEN [dotfiles] $CLEAR \n"
+
+echo "$GREEN Install Desktop or Server config files? [d/s] $CLEAR"
 read ANSWER
 
 echo -e "$GREEN Linking gitconfig to home $CLEAR"
-ln -s $SCRIPT_DIR/.gitconfig ~/.gitconfig
+ln -s $ROOT_DIR/.gitconfig ~/.gitconfig
 
 echo -e "$GREEN Linking tmux config to home $CLEAR"
-ln -s $SCRIPT_DIR/.tmux.conf ~/.tmux.conf
-
-echo -e "$GREEN Linking vimrc config to home $CLEAR"
-ln -s $SCRIPT_DIR/.vimrc ~/.vimrc
+ln -s $ROOT_DIR/.tmux.conf ~/.tmux.conf
 
 # config folders
 mkdir -p ~/.config
 
-echo -e "$GREEN Linking Alacritty config folder $CLEAR"
-ln -s $SCRIPT_DIR/.config/alacritty ~/.config/alacritty
-
-echo -e "$GREEN Linking Neofetch config folder $CLEAR"
-ln -s $SCRIPT_DIR/.config/neofetch ~/.config/neofetch
+echo -e "$GREEN Linking neovim config folder $CLEAR"
+ln -s $ROOT_DIR/.config/nvim ~/.config/nvim
 
 if [ "$ANSWER" == "d" ]; then
 
-	echo -e "$GREEN Linking neovim config folder $CLEAR"
-	ln -s $SCRIPT_DIR/.config/nvim ~/.config/nvim
+	echo -e "$GREEN Linking Alacritty config folder $CLEAR"
+	ln -s $ROOT_DIR/.config/alacritty ~/.config/alacritty
+
+	echo -e "$GREEN Linking i3wm config folder $CLEAR"
+	ln -s $ROOT_DIR/.config/i3 ~/.config/i3
 
 	echo -e "$GREEN Linking MangoHud config folder $CLEAR"
-	ln -s $SCRIPT_DIR/.config/MangoHud ~/.config/MangoHud
+	ln -s $ROOT_DIR/.config/MangoHud ~/.config/MangoHud
 
 	echo -e "$GREEN Linking vkBasalt config folder $CLEAR"
-	ln -s $SCRIPT_DIR/.config/vkBasalt ~/.config/vkBasalt
+	ln -s $ROOT_DIR/.config/vkBasalt ~/.config/vkBasalt
 
 fi
 
-echo -e "$GREEN Done. $CLEAR"
+echo -e "$GREEN [environment] $CLEAR \n"
+$SCRIPT_DIR/./env/bashrc-config.sh
+$SCRIPT_DIR/./env/install-scripts.sh
+
+echo -e "$GREEN [programs] $CLEAR \n"
+$SCRIPT_DIR/./installers/fastfetch.sh
+$SCRIPT_DIR/./installers/fonts.sh
+$SCRIPT_DIR/./installers/nvim.sh
+$SCRIPT_DIR/./installers/rust.sh
+$SCRIPT_DIR/./installers/rust-apps.sh
+
+echo -e "$GREEN Done! $CLEAR"
 
