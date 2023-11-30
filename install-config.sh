3a3c9600bd42a181c4f88802f1c090f6fb067ba4
Matheus Souza <mfbsouza.it@gmail.com>
2023-11-30 16:22:50 -0300

    Added arch-setup for gnome, updated install-config, bashrc and fonts
    Signed-off-by: Matheus Souza <mfbsouza.it@gmail.com>

--- install-config.sh
+++ install-config.sh
@@ -7,96 +7,154 @@ ROOT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SCRIPT_DIR=$ROOT_DIR/scripts
 
 echo -e "$GREEN [dotfiles] $CLEAR"
-
-echo -e "$GREEN Install Desktop or Server config files? [d/s] $CLEAR"
-read ANSWER
-
-echo -e "$GREEN Linking gitconfig to home $CLEAR"
-ln -s $ROOT_DIR/.gitconfig ~/.gitconfig
-
-echo -e "$GREEN Linking tmux config to home $CLEAR"
-ln -s $ROOT_DIR/.tmux.conf ~/.tmux.conf
-
 mkdir -p ~/.config
+mkdir -p ~/.config/environment.d
 
-echo -e "$GREEN Linking neovim config folder $CLEAR"
-ln -s $ROOT_DIR/.config/nvim ~/.config/nvim
+echo -e "$GREEN Install .gitconfig? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	echo -e "$GREEN Linking gitconfig to home $CLEAR"
+	ln -s $ROOT_DIR/.gitconfig ~/.gitconfig
+fi
 
-if [ "$ANSWER" == "d" ]; then
+echo -e "$GREEN Install .tmux? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	echo -e "$GREEN Linking tmux config to home $CLEAR"
+	ln -s $ROOT_DIR/.tmux.conf ~/.tmux.conf
+fi
 
+echo -e "$GREEN Install neovim config? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	echo -e "$GREEN Linking neovim config folder $CLEAR"
+	ln -s $ROOT_DIR/.config/nvim ~/.config/nvim
+fi
+
+echo -e "$GREEN Install custom fonts? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
 	echo -e "$GREEN Linking fonts folder $CLEAR"
 	ln -s $ROOT_DIR/.fonts ~/.fonts
 	fc-cache -fv
+fi
 
+echo -e "$GREEN Install alacritty config? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
 	echo -e "$GREEN Linking Alacritty config folder $CLEAR"
 	ln -s $ROOT_DIR/.config/alacritty ~/.config/alacritty
+fi
 
+echo -e "$GREEN Install MangoHud config? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
 	echo -e "$GREEN Linking MangoHud config folder $CLEAR"
 	ln -s $ROOT_DIR/.config/MangoHud ~/.config/MangoHud
+fi
 
+echo -e "$GREEN Install vkBasalt config? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
 	echo -e "$GREEN Linking vkBasalt config folder $CLEAR"
 	ln -s $ROOT_DIR/.config/vkBasalt ~/.config/vkBasalt
+fi
 
+echo -e "$GREEN Install ssh and git environment variables? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
 	echo -e "$GREEN Linking environment configs to environment.d $CLEAR"
-	mkdir -p ~/.config/environment.d
 	ln -s $ROOT_DIR/.config/environment.d/ssh_auth_socket.conf \
 		~/.config/environment.d/ssh_auth_socket.conf
 	ln -s $ROOT_DIR/.config/environment.d/ssh_askpass.conf \
 		~/.config/environment.d/ssh_askpass.conf
 	ln -s $ROOT_DIR/.config/environment.d/git_askpass.conf \
 		~/.config/environment.d/git_askpass.conf
+fi
 
+echo -e "$GREEN Install ssh-agent to autostart? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
 	echo -e "$GREEN Linking ssh-add.desktop to autostart $CLEAR"
 	mkdir -p ~/.config/autostart
 	ln -s $ROOT_DIR/.config/autostart/ssh-add.desktop \
 		~/.config/autostart/ssh-add.desktop
+fi
 
+echo -e "$GREEN Install ssh-agent systemd service? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
 	echo -e "$GREEN Linking systemd user services folder $CLEAR"
 	ln -s $ROOT_DIR/.config/systemd/user/ssh-agent.service \
 		~/.config/systemd/user/ssh-agent.service
 
 	echo -e "$GREEN Enabling ssh-agent user service $CLEAR"
 	systemctl --user enable ssh-agent.service
+fi
 
-	echo -e "$GREEN Install Intel GPU Tweaks? [y/n] $CLEAR"
-	read ANS
-	if [ "$ANS" == "y" ]; then
-		echo dev.i915.perf_stream_paranoid=0 \
-			| sudo tee -a /etc/sysctl.d/99-i915psp.conf
-	fi
+echo -e "$GREEN Install firefox wayland environment variable? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	ln -s $ROOT_DIR/.config/environment.d/firefox_wayland.conf \
+		~/.config/environment.d/firefox_wayland.conf
+fi
 
-	echo -e "$GREEN Install NVIDIA VAAPI config? [y/n] $CLEAR"
-	read ANS
-	if [ "$ANS" == "y" ]; then
-		ln -s $ROOT_DIR/.config/environment.d/nvidia_vaapi.conf \
-			~/.config/environment.d/nvidia_vaapi.conf
-	fi
+echo -e "$GREEN Install Intel GPU Tweaks? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	echo dev.i915.perf_stream_paranoid=0 \
+		| sudo tee -a /etc/sysctl.d/99-i915psp.conf
+fi
 
-	echo -e "$GREEN Install NVIDIA NVDEC config for mpv? [y/n] $CLEAR"
-	read ANS
-	if [ "$ANS" == "y" ]; then
-		mkdir -p ~/.config/mpv
-		ln -s $ROOT_DIR/.config/mpv/mpv-nvidia.conf \
-			~/.config/mpv/mpv.conf
-	fi
+echo -e "$GREEN Install NVIDIA VAAPI config? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	ln -s $ROOT_DIR/.config/environment.d/nvidia_vaapi.conf \
+		~/.config/environment.d/nvidia_vaapi.conf
+fi
 
-	echo -e "$GREEN Install Intel VAAPI config for mpv? [y/n] $CLEAR"
-	read ANS
-	if [ "$ANS" == "y" ]; then
-		mkdir -p ~/.config/mpv
-		ln -s $ROOT_DIR/.config/mpv/mpv-intel.conf \
-			~/.config/mpv/mpv.conf
-	fi
+echo -e "$GREEN Install NVIDIA NVDEC config for mpv? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	mkdir -p ~/.config/mpv
+	ln -s $ROOT_DIR/.config/mpv/mpv-nvidia.conf \
+		~/.config/mpv/mpv.conf
+fi
 
+echo -e "$GREEN Install Intel VAAPI config for mpv? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	mkdir -p ~/.config/mpv
+	ln -s $ROOT_DIR/.config/mpv/mpv-intel.conf \
+		~/.config/mpv/mpv.conf
 fi
 
 echo -e "\n$GREEN [environment] $CLEAR"
-$SCRIPT_DIR/./env/bashrc-config.sh
-$SCRIPT_DIR/./env/install-scripts.sh
+
+echo -e "$GREEN Install bashrc config? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	$SCRIPT_DIR/./env/bashrc-config.sh
+fi
+
+echo -e "$GREEN Install scripts to the path? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	$SCRIPT_DIR/./env/install-scripts.sh
+fi
 
 echo -e "\n$GREEN [theming] $CLEAR"
-$SCRIPT_DIR/./installers/sddm-theme-catppuccin.sh
-$SCRIPT_DIR/./installers/plasma-theme-catppuccin.sh
+
+echo -e "$GREEN Install sddm catppuccin theme? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	$SCRIPT_DIR/./installers/sddm-theme-catppuccin.sh
+fi
+
+echo -e "$GREEN Install plasma catppuccin theme? [y/n] $CLEAR"
+read ANS
+if [ "$ANS" == "y" ]; then
+	$SCRIPT_DIR/./installers/plasma-theme-catppuccin.sh
+fi
 
 echo -e "$GREEN Done! $CLEAR"
-
---
+++ scripts/archlinux/setup-gnome.sh
@@ -0,0 +1,205 @@
+#!/bin/bash
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+CLEAR='\033[0m'
+
+
+echo ""
+echo -e "$GREEN Installing Packages... $CLEAR"
+echo ""
+
+sudo pacman -S gnome flatpak d-spy endeavour file-roller geary gedit ghex \
+	gitg sysprof firefox alacritty tmux neovim openssh wget tree picocom htop \
+	bash-completion rsync fastfetch stress usbutils ethtool inxi nmap less \
+	cpupower dhcpcd net-tools dnsmasq iptables-nft dnsutils turbostat swtpm \
+	mpv virt-manager wireshark-qt dbeaver qemu-desktop transmission-gtk \
+	nasm go gopls valgrind meson cmake clang llvm openmp rustup rust-analyzer \
+	jre-openjdk docker sqlite lcov python-pip python-setuptools python-pylint \
+	bear nodejs npm perl-json-xs fwupd helvum libva-utils vdpauinfo \
+	openvpn wireguard-tools systemd-resolvconf usb_modeswitch \
+	networkmanager-openvpn networkmanager-l2tp networkmanager-strongswan \
+	wl-clipboard mailcap xclip xsel gperf help2man diffstat chrpath \
+	rpcsvc-proto inetutils spirv-headers \
+	noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-liberation
+if [ "$?" -ne 0 ]; then
+	echo ""
+	echo -e "$RED Something went wrong! Stopping... $CLEAR"
+	echo ""
+	exit 1
+fi
+
+echo ""
+echo -e "$GREEN Installing rust default toolchain... $CLEAR"
+echo ""
+
+rustup default stable
+if [ "$?" -ne 0 ]; then
+	echo ""
+	echo -e "$RED Something went wrong! Stopping... $CLEAR"
+	echo ""
+	exit 1
+fi
+
+echo ""
+echo -e "$GREEN Installing rust apps... $CLEAR"
+echo ""
+
+cargo install ripgrep bat fd-find loc
+if [ "$?" -ne 0 ]; then
+	echo ""
+	echo -e "$RED Something went wrong! Stopping... $CLEAR"
+	echo ""
+	exit 1
+fi
+
+
+echo -e "$GREEN Install nvidia graphics packages? [y/n] $CLEAR"
+read ANSWER
+if [ "$ANSWER" == "y" ]; then
+	sudo pacman -S opencl-nvidia cuda nvidia-settings nvidia-utils \
+		libva-nvidia-driver
+	if [ "$?" -ne 0 ]; then
+		echo ""
+		echo -e "$RED Something went wrong! Stopping... $CLEAR"
+		echo ""
+		exit 1
+	fi
+fi
+
+echo -e "$GREEN Install amd graphics packages? [y/n] $CLEAR"
+read ANSWER
+if [ "$ANSWER" == "y" ]; then
+	sudo pacman -S mesa vulkan-radeon libva-mesa-driver opencl-mesa \
+		lib32-vulkan-radeon
+	if [ "$?" -ne 0 ]; then
+		echo ""
+		echo -e "$RED Something went wrong! Stopping... $CLEAR"
+		echo ""
+		exit 1
+	fi
+fi
+
+echo -e "$GREEN Install intel graphics packages? [y/n] $CLEAR"
+read ANSWER
+if [ "$ANSWER" == "y" ]; then
+	sudo pacman -S mesa vulkan-intel intel-media-driver intel-compute-runtime \
+		lib32-vulkan-intel intel-gpu-tools
+	if [ "$?" -ne 0 ]; then
+		echo ""
+		echo -e "$RED Something went wrong! Stopping... $CLEAR"
+		echo ""
+		exit 1
+	fi
+fi
+
+echo -e "$GREEN Install intel undervolt tool? [y/n] $CLEAR"
+read ANSWER
+if [ "$ANSWER" == "y" ]; then
+	sudo pacman -S intel-undervolt
+	if [ "$?" -ne 0 ]; then
+		echo ""
+		echo -e "$RED Something went wrong! Stopping... $CLEAR"
+		echo ""
+		exit 1
+	fi
+fi
+
+echo -e "$GREEN Install nvidia prime switch tool? [y/n] $CLEAR"
+read ANSWER
+if [ "$ANSWER" == "y" ]; then
+	sudo pacman -S nvidia-prime
+	if [ "$?" -ne 0 ]; then
+		echo ""
+		echo -e "$RED Something went wrong! Stopping... $CLEAR"
+		echo ""
+		exit 1
+	fi
+fi
+
+echo -e "$GREEN Install battery managment tools? [y/n] $CLEAR"
+read ANSWER
+if [ "$ANSWER" == "y" ]; then
+	sudo pacman -S tlp tlp-rdw
+	if [ "$?" -ne 0 ]; then
+		echo ""
+		echo -e "$RED Something went wrong! Stopping... $CLEAR"
+		echo ""
+		exit 1
+	fi
+	echo -e "$GREEN enabling TLP service... $CLEAR"
+	sudo systemctl enable tlp
+	sudo systemctl enable NetworkManager-dispatcher.service
+	sudo systemctl mask systemd-rfkill.service
+	sudo systemctl mask systemd-rfkill.socket
+fi
+
+echo -e "$GREEN Set cpu to performance governor? [y/n] $CLEAR"
+read ANSWER
+if [ "$ANSWER" == "y" ]; then
+	sudo sed -i "s/#governor='ondemand'/governor='performance'/g" \
+		/etc/default/cpupower
+	if [ "$?" -ne 0 ]; then
+		echo ""
+		echo -e "$RED Something went wrong! Stopping... $CLEAR"
+		echo ""
+		exit 1
+	fi
+	echo -e "$GREEN enabling cpupower service... $CLEAR"
+	sudo systemctl enable cpupower.service
+fi
+
+echo ""
+echo -e "$GREEN Installing yay... $CLEAR"
+echo ""
+ git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
+ cd /tmp/yay-bin
+ makepkg -si
+ cd -
+
+echo ""
+echo -e "$GREEN Installing downgrade utility... $CLEAR"
+echo ""
+yay -S downgrade
+
+echo ""
+echo -e "$GREEN enabling systemd services... $CLEAR"
+echo ""
+sudo systemctl enable gdm
+sudo systemctl enable systemd-resolved
+sudo systemctl enable bluetooth
+sudo systemctl enable sshd
+sudo systemctl enable ModemManager
+sudo systemctl enable docker
+sudo systemctl enable libvirtd
+
+echo ""
+echo -e "$GREEN Adding user to groups... $CLEAR"
+echo ""
+
+sudo usermod -a -G wireshark $USER
+if [ "$?" -ne 0 ]; then
+	echo ""
+	echo -e "$RED Something went wrong! Stopping... $CLEAR"
+	echo ""
+	exit 1
+fi
+sudo usermod -a -G docker $USER
+if [ "$?" -ne 0 ]; then
+	echo ""
+	echo -e "$RED Something went wrong! Stopping... $CLEAR"
+	echo ""
+	exit 1
+fi
+sudo usermod -a -G uucp $USER
+if [ "$?" -ne 0 ]; then
+	echo ""
+	echo -e "$RED Something went wrong! Stopping... $CLEAR"
+	echo ""
+	exit 1
+fi
+
+echo ""
+echo -e "$GREEN Done! $CLEAR"
+echo ""
+exit 0
