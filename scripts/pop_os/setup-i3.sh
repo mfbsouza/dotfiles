b64205a1d172e33af35d93a6f9e939db8370c300
Matheus Souza <mfbsouza.it@gmail.com>
2024-01-02 15:44:59 -0300

    pop_os: added script for installing my programs
    Signed-off-by: Matheus Souza <mfbsouza.it@gmail.com>

---
+++ scripts/pop_os/setup-i3.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+sudo apt install -y i3-wm i3lock i3status suckless-tools j4-dmenu-desktop \
+	alacritty tmux htop tree bear fd-bind ripgrep neofetch maim \
+	mpv pavucontrol blueman vainfo ninja-build gettext cmake unzip curl \
+	ffmpegthumnailer fonts-noto \
