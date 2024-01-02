efa8124437cb3a4e0ac2ba3087dd2bb170595031
Matheus Souza <mfbsouza.it@gmail.com>
2024-01-02 15:47:16 -0300

    pop_os: added feh
    Signed-off-by: Matheus Souza <mfbsouza.it@gmail.com>

--- scripts/pop_os/setup-i3.sh
+++ scripts/pop_os/setup-i3.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt install -y i3-wm i3lock i3status suckless-tools j4-dmenu-desktop \
-	alacritty tmux htop tree bear fd-bind ripgrep neofetch maim \
+	alacritty tmux htop tree bear fd-bind ripgrep neofetch feh maim \
 	mpv pavucontrol blueman vainfo ninja-build gettext cmake unzip curl \
 	ffmpegthumnailer fonts-noto \
