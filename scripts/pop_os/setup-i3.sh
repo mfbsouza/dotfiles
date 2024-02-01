ded68c2e7e4f8fd8d05e42c495d35af8e630e302
Matheus Souza <mfbsouza.it@gmail.com>
2024-02-01 14:13:30 -0300

    removed unused config and scripts
    Signed-off-by: Matheus Souza <mfbsouza.it@gmail.com>

--- scripts/pop_os/setup-i3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sudo apt install -y i3-wm i3lock i3status suckless-tools j4-dmenu-desktop \
-	alacritty tmux htop fzf tree bear fd-bind ripgrep neofetch feh maim \
-	mpv pavucontrol blueman vainfo ninja-build gettext cmake unzip curl \
-	transmission ffmpegthumnailer fonts-noto \
-
-flatpak install telegram discord zoom teams
