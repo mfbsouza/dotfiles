2a7f0e14d3364a34a9d850e73cba41e1699c6ddf
Matheus Souza <mfbsouza.it@gmail.com>
2024-02-16 22:11:34 -0300

    setup-i3: improved modularity of the script
    Signed-off-by: Matheus Souza <mfbsouza.it@gmail.com>

--- scripts/archlinux/setup-i3.sh
+++ scripts/archlinux/setup-i3.sh
@@ -5,273 +5,319 @@ RED='\033[0;31m'
 CLEAR='\033[0m'
 
 
-echo ""
-echo -e "$GREEN Installing Packages... $CLEAR"
-echo ""
+echo -e "\n$GREEN Arch Linux setup personal script by mfbsouza $CLEAR\n"
 
-sudo pacman -S xorg xorg-xinit xsel xclip noto-fonts noto-fonts-cjk \
-	noto-fonts-emoji ttf-liberation ttf-inconsolata-nerd wget unzip \
-	htop usbutils rsync less fzf fastfetch i3 j4-dmenu-desktop feh \
-	picom xdg-utils xdg-user-dirs blueman bluez-utils gnome-keyring \
-	dmenu alacritty tmux gdb fd ripgrep go rustup firefox flatpak mpv \
-	thunar thunar-archive-plugin xarchiver tumbler lxrandr lxappearance \
-	gnome-themes-extra gvfs udiskie bash-completion ethtool smartmontools \
-	pavucontrol ffmpegthumbnailer gst-libav gst-plugins-ugly xss-lock \
-	libva-utils gpicview xdotool maim brightnessctl transmission-gtk \
-	cpupower docker wireshark-qt virt-manager qemu-desktop jre-openjdk \
-	xdg-desktop-portal-gtk nm-connection-editor volumeicon kdenlive \
-	xterm dunst filelight network-manager-applet openvpn ttf-opensans \
-	networkmanager-openvpn networkmanager-l2tp networkmanager-strongswan \
-	systemd-resolvconf lcov python-pip python-pylint wireguard-tools \
-	valgrind bear fwupd dhcpcd inxi net-tools dnsmasq dnsutils swtpm \
-	openmp helvum gperf help2man diffstat chrpath rpcsvc-proto inetutils \
-	breeze breeze-gtk boost
-if [ "$?" -ne 0 ]; then
-	echo ""
-	echo -e "$RED Something went wrong! Stopping... $CLEAR"
-	echo ""
-	exit 1
+echo -e "\n$GREEN Install basic terminal packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S bash-completion git openssh rsync less fzf \
+		htop tmux fd ripgrep usbutils fastfetch vim wget unzip \
+		inxi net-tools
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
 fi
 
-echo ""
-echo -e "$GREEN Installing rust default toolchain... $CLEAR"
-echo ""
-
-rustup default stable
-if [ "$?" -ne 0 ]; then
-	echo ""
-	echo -e "$RED Something went wrong! Stopping... $CLEAR"
-	echo ""
-	exit 1
+echo -e "\n$GREEN Install i3wm desktop packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S xorg xorg-xinit i3 dmenu j4-dmenu-desktop picom \
+		dunst feh alacritty firefox thunar thunar-archive-plugin \
+		gvfs udiskie tumbler ffmpegthumbnailer xarchiver lxrandr \
+		lxappearance pavucontrol volumeicon gpicview maim xdotool \
+		xdg-utils xdg-user-dirs xdg-desktop-portal-gtk blueman \
+		bluez-utils gnome-keyring network-manager-applet filelight \
+		nm-connection-editor xss-lock systemd-resolvconf breeze \
+		breeze-gtk breeze-icons brightnessctl
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
 fi
 
-echo -e "$GREEN Install nvidia graphics packages? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install VPN packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S openvpn networkmanager-openvpn networkmanager-l2tp \
+		networkmanager-strongswan wireguard-tools
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install software development packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S gdb go rustup bear valgrind neovim xsel xclip \
+		python-pip jre-openjdk docker lcov
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+	rustup default stable
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install virtualization packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S virt-manager qemu-desktop
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install mpv and multimedia codecs? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S mpv gst-libav gst-plugins-ugly libva-utils
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install fonts? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S noto-fonts noto-fonts-cjk noto-fonts-emoji \
+		ttf-liberation ttf-inconsolata-nerd
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install transmission-gtk package? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S transmission-gtk
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install cpupower package? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S cpupower
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install flatpak? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S flatpak
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install helvum? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo pacman -S helvum
+	if [ "$?" -ne 0 ]; then
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
+		exit 1
+	fi
+fi
+
+echo -e "\n$GREEN Install nvidia graphics packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S opencl-nvidia cuda nvidia-utils libva-nvidia-driver
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install nvidia lib32 packages and nvidia settings? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install nvidia lib32 packages and nvidia settings? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S nvidia-settings lib32-nvidia-utils
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install amd graphics packages? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install amd graphics packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S mesa vulkan-radeon libva-mesa-driver opencl-mesa
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install amdgpu lib32 packages? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install amdgpu lib32 packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S lib32-vulkan-radeon
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install intel graphics packages? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install intel graphics packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S mesa vulkan-intel intel-media-driver intel-compute-runtime \
 		intel-gpu-tools
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install intel graphics lib32 packages? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install intel graphics lib32 packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S lib32-vulkan-intel
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install gaming packages? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install gaming packages? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S mangohud steam wine-staging winetricks lutris
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install intel undervolt tool? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install intel undervolt tool? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S intel-undervolt
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install nvidia prime switch tool? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install nvidia prime switch tool? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S nvidia-prime
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
 fi
 
-echo -e "$GREEN Install battery managment tools? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install TLP for power managment? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo pacman -S tlp tlp-rdw
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
-	echo -e "$GREEN enabling TLP service... $CLEAR"
+	echo -e "\n$GREEN enabling TLP service... $CLEAR\n"
 	sudo systemctl enable tlp
 	sudo systemctl enable NetworkManager-dispatcher.service
 	sudo systemctl mask systemd-rfkill.service
 	sudo systemctl mask systemd-rfkill.socket
 fi
 
-echo -e "$GREEN Set cpu to performance governor? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Set cpu to performance governor? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	sudo sed -i "s/#governor='ondemand'/governor='performance'/g" \
 		/etc/default/cpupower
 	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
+		echo -e "\n$RED Something went wrong! Stopping... $CLEAR\n"
 		exit 1
 	fi
-	echo -e "$GREEN enabling cpupower service... $CLEAR"
+	echo -e "\n$GREEN enabling cpupower service... $CLEAR\n"
 	sudo systemctl enable cpupower.service
 fi
 
-echo ""
-echo -e "$GREEN Installing yay... $CLEAR"
-echo ""
- git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
- cd /tmp/yay-bin
- makepkg -si
- cd -
 
-echo ""
-echo -e "$GREEN Installing downgrade utility... $CLEAR"
-echo ""
-yay -S downgrade
-
-echo -e "$GREEN install visual studio code? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
-	yay -S visual-studio-code-bin
-	# fix mime vscode "bug"
-	# xdg-mime default pcmanfm.desktop inode/directory
+echo -e "\n$GREEN Install yay for AUR package managment? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	git clone https://aur.archlinux.org/yay-bin.git /tmp/yay-bin
+	cd /tmp/yay-bin
+	makepkg -si
+	cd -
 fi
 
-echo -e "$GREEN install google chrome? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install downgrade utility? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	yay -S downgrade
+fi
+
+# fix mime vscode "bug"
+# xdg-mime default thunar.desktop inode/directory
+echo -e "\n$GREEN Install visual studio code? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	yay -S visual-studio-code-bin
+fi
+
+echo -e "\n$GREEN Install google chrome? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	yay -S google-chrome
 fi
 
-echo -e "$GREEN install Saleae Logic2? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
+echo -e "\n$GREEN Install Saleae Logic2? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
 	yay -S saleae-logic2
 fi
 
-echo ""
-echo -e "$GREEN enabling systemd services... $CLEAR"
-echo ""
-sudo systemctl enable systemd-resolved
-sudo systemctl enable bluetooth
-sudo systemctl enable sshd
-sudo systemctl enable docker
-sudo systemctl enable libvirtd
-
-echo ""
-echo -e "$GREEN Adding user to groups... $CLEAR"
-echo ""
-
-sudo usermod -a -G docker $USER
-if [ "$?" -ne 0 ]; then
-	echo ""
-	echo -e "$RED Something went wrong! Stopping... $CLEAR"
-	echo ""
-	exit 1
+echo -e "\n$GREEN Enable systemd services? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo systemctl enable systemd-resolved
+	sudo systemctl enable bluetooth
+	sudo systemctl enable sshd
+	sudo systemctl enable docker
+	sudo systemctl enable libvirtd
 fi
 
-sudo usermod -a -G uucp $USER
-if [ "$?" -ne 0 ]; then
-	echo ""
-	echo -e "$RED Something went wrong! Stopping... $CLEAR"
-	echo ""
-	exit 1
+echo -e "\n$GREEN Adding user to groups... $CLEAR\n"
+
+echo -e "\n$GREEN Add user to docker group? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo usermod -a -G docker $USER
 fi
 
-sudo usermod -a -G wireshark $USER
-if [ "$?" -ne 0 ]; then
-	echo ""
-	echo -e "$RED Something went wrong! Stopping... $CLEAR"
-	echo ""
-	exit 1
+echo -e "\n$GREEN Add user to uucp group? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo usermod -a -G uucp $USER
 fi
 
-echo -e "$GREEN flatpak apps? [y/n] $CLEAR"
-read ANSWER
-if [ "$ANSWER" == "y" ]; then
-	flatpak install discord zoom telegram postman
-	# postman fix
-	# path: .var/app/com.getpostman.Postman/config/Postman/proxy/
-	# openssl req -subj '/C=US/CN=Postman Proxy' -new -newkey rsa:2048 -sha256 -days 365 -nodes -x509 -keyout postman-proxy-ca.key -out postman-proxy-ca.crt
-	if [ "$?" -ne 0 ]; then
-		echo ""
-		echo -e "$RED Something went wrong! Stopping... $CLEAR"
-		echo ""
-		exit 1
-	fi
+echo -e "\n$GREEN Add user to wireshark group? [y/N] $CLEAR\n"
+read ANS
+if [ "$ANS" == "y" ]; then
+	sudo usermod -a -G wireshark $USER
 fi
 
-echo ""
-echo -e "$GREEN Done! $CLEAR"
-echo ""
+echo -e "\n$GREEN Done! $CLEAR\n"
 exit 0
