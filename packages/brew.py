6e580231bd2ef9a58f78f8474e947fcbeccae3c0
Matheus Souza <mfbsouza.it@gmail.com>
2025-04-12 10:29:04 -0300

    Added jetbrains nerd fonts to brew packages

--- packages/brew.py
+++ packages/brew.py
@@ -3,7 +3,7 @@ installCmd = "brew install"
 cliTui = ("git git-lfs tmux tree htop fastfetch neovim ripgrep fd fzf ")
 coding = ("oven-sh/bun/bun zig go python virtualenv pyenv pyenv-virtualenv basedpyright node ")
 keyring = ("gpg pinentry-mac ")
-cask = ("ghostty dbeaver-community postman slack visual-studio-code zed zoom ")
+cask = ("ghostty dbeaver-community postman slack visual-studio-code zed zoom font-jetbrains-mono-nerd-font ")
 
 linuxPkgs = (cliTui + coding)
 macPkgs = (cliTui + coding + keyring)
