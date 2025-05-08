a1851d4809a5e47bfbaf489ac0bec233154ef519
Matheus Souza <mfbsouza.it@gmail.com>
2025-05-08 20:28:10 -0300

    Updated wezterm color scheme settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -7,7 +7,15 @@ config.font_size = 10.0
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
-config.color_scheme = 'midnight-in-mojave'
+-- customize default color scheme
+local custom_theme = wezterm.color.get_builtin_schemes()['midnight-in-mojave'];
+custom_theme.ansi[1] = '#333333'
+custom_theme.brights[1] = '#3d3d3d'
+
+config.color_schemes = {
+  ['Midnight in Mojave'] = custom_theme,
+}
+config.color_scheme = 'Midnight in Mojave';
 
 return config
 
