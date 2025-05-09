5a34d3e72cc0380dc8e33591e008208419df493c
Matheus Souza <mfbsouza.it@gmail.com>
2025-05-08 23:18:52 -0300

    Disabled font ligature on wezterm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,9 @@ local config = wezterm.config_builder()
 config.font = wezterm.font 'JetBrains Mono'
 config.font_size = 10.0
 
+-- disable ligatures
+config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
+
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 
