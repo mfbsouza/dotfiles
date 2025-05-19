7a32f8d22bb09b82e8d4a8d82dfecfdbe9ba3fa9
Matheus Souza <mfbsouza.it@gmail.com>
2025-05-19 11:17:54 -0300

    MacOS specific config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 10.0
+config.font_size = 14.0
 
 -- disable ligatures
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
@@ -19,6 +19,8 @@ config.color_schemes = {
   ['Midnight in Mojave'] = custom_theme,
 }
 config.color_scheme = 'Midnight in Mojave';
+
+config.audible_bell = "Disabled"
 
 return config
 
