fe0f330251f4313b5803c9db8ce6ecee4970912e
Matheus Souza <mfbsouza.it@gmail.com>
2025-06-26 09:11:27 -0300

    brew: added gcc, llvm, nanocom

--- packages/brew.py
+++ packages/brew.py
@@ -1,9 +1,9 @@
 installCmd = "brew install"
 
-cliTui = ("git git-lfs tmux tree htop fastfetch neovim ripgrep fd fzf ")
-coding = ("oven-sh/bun/bun zig go python virtualenv pyenv pyenv-virtualenv basedpyright node ")
+cliTui = ("git git-lfs tmux tree htop fastfetch neovim ripgrep fd fzf nanocom ")
+coding = ("oven-sh/bun/bun gcc llvm zig go python virtualenv pyenv pyenv-virtualenv basedpyright node ")
 keyring = ("gpg pinentry-mac ")
-cask = ("ghostty dbeaver-community postman slack visual-studio-code zed zoom font-jetbrains-mono-nerd-font ")
+cask = ("wezterm dbeaver-community postman slack visual-studio-code zed zoom font-jetbrains-mono-nerd-font ")
 
 linuxPkgs = (cliTui + coding)
 macPkgs = (cliTui + coding + keyring)
