191fc8f235c0d14fdc1abd8aa66adb63282bdd05
Matheus Souza <mfbsouza.it@gmail.com>
2025-07-17 10:23:25 -0300

    renamed setup script and update packages file

--- packages/brew.py
+++ packages/brew.py
@@ -1,10 +1,9 @@
 installCmd = "brew install"
 
-cliTui = ("git git-lfs tmux tree htop fastfetch neovim ripgrep fd fzf nanocom ")
-coding = ("oven-sh/bun/bun gcc llvm zig go python virtualenv pyenv pyenv-virtualenv basedpyright node ")
+cliTui = ("git git-lfs tmux tree htop fastfetch neovim vim ripgrep fd fzf nanocom ")
+coding = ("oven-sh/bun/bun gcc llvm zig go python virtualenv pyenv pyenv-virtualenv basedpyright node lua ")
 keyring = ("gpg pinentry-mac ")
 cask = ("wezterm dbeaver-community postman slack visual-studio-code zed zoom font-jetbrains-mono-nerd-font ")
 
 linuxPkgs = (cliTui + coding)
 macPkgs = (cliTui + coding + keyring)
-
--- packages/mint.py
+++ packages/mint.py
@@ -1,3 +1,5 @@
 installCmd = "sudo apt -y install"
 
-sys = ("zsh vim default-jre openssh-server virt-manager xclip git git-lfs tmux tree htop ripgrep fd-find nodejs npm python3-pip python3-venv virtualenv clang llvm clangd clang-format cmake ")
+sys = ("zsh vim default-jre openssh-server virt-manager xclip git git-lfs tmux "
+	"tree htop ripgrep fd-find nodejs npm python3-pip python3-venv virtualenv "
+	"clang llvm clangd clang-format cmake ")
