4c7c41ed028da3ee77a0e3a124d0da9b8e0e2bc8
Matheus Souza <mfbsouza.it@gmail.com>
2025-07-25 09:59:47 -0300

    wezterm: added keybind for moving by words

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm'
 local config = wezterm.config_builder()
 
 config.font = wezterm.font 'JetBrains Mono'
-config.font_size = 10.0
+config.font_size = 14.0
 
 -- disable ligatures
 config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
@@ -19,6 +19,16 @@ config.color_schemes = {
   ['Midnight in Mojave'] = custom_theme,
 }
 config.color_scheme = 'Midnight in Mojave';
+
+config.audible_bell = "Disabled"
+
+-- keybinds
+config.keys = {
+  -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+  {key="LeftArrow", mods="OPT", action=wezterm.action{SendString="\x1bb"}},
+  -- Make Option-Right equivalent to Alt-f; forward-word
+  {key="RightArrow", mods="OPT", action=wezterm.action{SendString="\x1bf"}},
+}
 
 return config
 
