ade4c1c060729179714cd5030a5b10d67f164640
Matheus Souza <mfbsouza.it@gmail.com>
2025-09-05 23:36:02 -0300

    nvim: added more keymaps

--- .config/nvim/lua/config/keymap.lua
+++ .config/nvim/lua/config/keymap.lua
@@ -17,11 +17,17 @@ keymap("n", "N", "Nzzzv")
 -- clear highlight
 keymap("n", "<Leader>h", ":noh<CR>", opts)
 
+-- netrw on the left side
+keymap("n", "<C-e>", ":Lex 30<CR>", opts)
+
 -- move text sideways and up-down
 keymap("v", "<S-Right>", ">gv", opts)
 keymap("v", "<S-Left>", "<gv", opts)
 keymap("v", "<S-Up>", ":m '<-2<CR>gv=gv", opts)
 keymap("v", "<S-Down>", ":m '>+1<CR>gv=gv", opts)
+
+-- Comment block of lines
+keymap("v", "gc", ":<C-u>'<,'>norm i", { noremap = true })
 
 -- don't save to register when deleting or changing in following cases
 keymap("v", "p", '"_dP', opts)
@@ -38,3 +44,6 @@ keymap("v", "<C-c>", '"+y', opts)
 
 -- double click to highlight all occurrences
 keymap("n", "<2-LeftMouse>", "*N", opts)
+
+-- fake zoom-in of the current window
+keymap("n", "<Leader>z", ":tab split<CR>", opts)
