9ba104f49f48a55d8c76dddecefd4bb8fc2ca0e5
Matheus Souza <mfbsouza.it@gmail.com>
2025-10-09 10:13:24 -0300

    nvim: added go to treesitter install

--- .config/nvim/lua/config/plugins/treesitter.lua
+++ .config/nvim/lua/config/plugins/treesitter.lua
@@ -7,7 +7,7 @@ return {
       local configs = require("nvim-treesitter.configs")
 
       configs.setup({
-        ensure_installed = { "c", "lua", "python" },
+        ensure_installed = { "c", "lua", "python", "go" },
         sync_install = false,
         highlight = { enable = true },
         indent = { enable = true },
