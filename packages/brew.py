57e1317142ad721e0c2d8ac5b4139cda5e777059
Matheus Souza <mfbsouza.it@gmail.com>
2025-12-13 15:09:17 -0300

    Minor brew packages changes

--- packages/brew.py
+++ packages/brew.py
@@ -1,15 +1,16 @@
 installCmd = "brew install"
 
+shell = "zsh "
 
 dev = ("gcc zig zls go gopls lua python node oven-sh/bun/bun "
-    "fastfetch neovim vim zsh ripgrep fd fzf ")
+    "fastfetch neovim vim ripgrep fd fzf btop bat ")
 
-cli = ("git git-lfs tmux tree htop nanocom pyenv pyenv-virtualenv ")
+cli = ("git git-lfs tmux tree nanocom pyenv pyenv-virtualenv ")
 
 keyring = ("gpg pinentry-mac ")
 
 cask = ("wezterm dbeaver-community postman slack visual-studio-code zed zoom "
 	"font-jetbrains-mono-nerd-font ")
 
-linuxPkgs = (dev)
+linuxPkgs = (shell + dev)
 macPkgs = (cli + dev + keyring)
