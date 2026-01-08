665ef31ca379e6af6baa05b73bd0b986fde6df8f
Matheus Souza <mfbsouza.it@gmail.com>
2026-01-07 21:11:28 -0300

    Snippet improvements for neovim and zed

--- .config/nvim/lua/plugin/blink.lua
+++ .config/nvim/lua/plugin/blink.lua
@@ -7,8 +7,8 @@ return {
     opts = {
       keymap = {
         preset = "default",
-        ["<Tab>"] = { "select_next", "fallback" },
-        ["<S-Tab>"] = { "select_prev", "fallback" },
+        ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
         ['<CR>'] = { 'accept', 'fallback' },
       },
       appearance = {
