a17a1244ffa828a52ecbb6aa29532033f1a829ca
Matheus Souza <mfbsouza.it@gmail.com>
2026-02-17 18:07:26 -0300

    c/c++ additions to package and zed config

--- packages/fedora.py
+++ packages/fedora.py
@@ -3,6 +3,6 @@ installCmd = "sudo dnf install"
 base = ("@development-tools ")
 sys = ("wl-clipboard tree htop btop fastfetch powertop ")
 editor = ("neovim vim ripgrep fd fzf bat ")
-programming = ("zig golang gopls node ")
+programming = ("zig golang gopls node clang clang-format ")
 
 pkgs = (sys + editor + programming)
