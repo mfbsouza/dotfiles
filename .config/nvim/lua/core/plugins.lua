581451258b291df1e262e0149ef9b022630d8702
Matheus Souza <mfbsouza.it@gmail.com>
2026-06-26 15:15:28 -0300

    nvim: custom func to display lsp diag in statusline

--- .config/nvim/lua/core/plugins.lua
+++ .config/nvim/lua/core/plugins.lua
@@ -176,6 +176,46 @@ if colorscheme == 'gruvbox' then
   custom_theme.command.c = custom_theme.normal.c
 end
 
+function GetCurrentDiagnostic()
+  bufnr = 0
+  line_nr = vim.api.nvim_win_get_cursor(0)[1] - 1
+  opts = { ["lnum"] = line_nr }
+
+  local line_diagnostics = vim.diagnostic.get(bufnr, opts)
+  if vim.tbl_isempty(line_diagnostics) then
+    return
+  end
+
+  local best_diagnostic = nil
+
+  for _, diagnostic in ipairs(line_diagnostics) do
+    if
+      best_diagnostic == nil or diagnostic.severity < best_diagnostic.severity
+    then
+      best_diagnostic = diagnostic
+    end
+  end
+
+  return best_diagnostic
+end
+
+function GetCurrentDiagnosticString()
+  local diagnostic = GetCurrentDiagnostic()
+
+  if not diagnostic or not diagnostic.message then
+    return
+  end
+
+  local message = vim.split(diagnostic.message, "\n")[1]
+  local max_width = vim.api.nvim_win_get_width(0) - 35
+
+  if string.len(message) < max_width then
+    return message
+  else
+    return string.sub(message, 1, max_width) .. "..."
+  end
+end
+
 require('lualine').setup({
   options = {
     theme = custom_theme,
@@ -201,6 +241,7 @@ require('lualine').setup({
     },
     lualine_x = {
       { 'lsp_status' },
+      { 'GetCurrentDiagnosticString()' },
       {
         'diagnostics',
         on_click = function()
